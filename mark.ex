0b2c74e8423f36e3c1f12a53d816d98ca4caa8be
t-sin <shinichi.tanaka45@gmail.com>
2017-07-27 17:09:29 +0900

    Adds a main module and basic functions

---
+++ mark.ex
@@ -0,0 +1,23 @@
+defmodule MarkLisp do
+  @moduledoc """
+  Lisp implementation to implement Mark the squirrel.
+  """
+
+  @doc """
+  Read expression as string.
+  """
+  def read(str) do
+  end
+
+  @doc """
+  Evaluate expression.
+  """
+  def eval(exp) do
+  end
+
+  @doc """
+  Print evaluation result.
+  """
+  def print(exp) do
+  end
+end
