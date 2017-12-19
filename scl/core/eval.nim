85fae3016f8e2bb4938cc616b126184532072ffa
t-sin <shinichi.tanaka45@gmail.com>
2017-12-19 13:49:52 +0900

    WIP: eval

---
+++ scl/core/eval.nim
@@ -0,0 +1,43 @@
+import objects
+import equality
+import print
+
+proc eval(obj: LispT): LispT =
+  if (obj of LispCharacter or
+      obj of LispNumber or
+      obj of LispNull or
+      obj of LispArray or
+      obj of LispVector or
+      obj of LispString):
+    return obj
+
+  if obj of LispSymbol:
+    var s = LispSymbol(obj)
+    return s.value
+
+  if obj of LispList:
+    var
+      c = LispList(obj)
+      fn = LispSymbol(c.car)
+      args = LispList(c.cdr)
+    if eq(fn, LispSymbol(name: "quote")) of LispT:
+      return c
+    if eq(fn, LispSymbol(name: "function")) of LispT:
+      return fn.function
+    else:
+      return fn
+
+  else:
+    echo "[otherwise!!]"
+    return obj
+
+import print
+
+when isMainModule:
+  var o = eval(
+    LispList(car: LispSymbol(name: "hogte"),
+             cdr: LispList (car: LispSymbol(name: "HOGE"),
+                            cdr: LispList(car: LispNull(),
+                                          cdr: LispNull()))))
+
+  echo write(o)
