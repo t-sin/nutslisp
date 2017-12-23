2c3ab330adcc78d29c79115e8061b0955a06e132
t-sin <shinichi.tanaka45@gmail.com>
2017-12-23 20:54:31 +0900

    `eval` looks up environments

--- scl/core/eval.nim
+++ scl/core/eval.nim
@@ -3,9 +3,11 @@ import print
 import pure
 
 
-proc eval(obj: LispT): LispT
+proc eval(env: LispEnvironment,
+          obj: LispT): LispT
 
-proc evalSetq(pairs: LispList): LispT =
+proc evalSetq(env: LispEnvironment,
+              pairs: LispList): LispT =
   if pairs.cdr of LispNil:
     raise newException(Exception, "invalid setq")
   else:
@@ -18,18 +20,20 @@ proc evalSetq(pairs: LispList): LispT =
     if rest.cdr of LispNull:
       return val
     else:
-      return evalSetq(LispList(rest.cdr))
+      return evalSetq(env, LispList(rest.cdr))
 
-proc parseLambdaList(args: LispList): LispList =
+proc parseLambdaList(env: LispEnvironment,
+                     args: LispList): LispList =
   if args.cdr of LispNull:
-    return LispList(car: eval(args.car),
+    return LispList(car: eval(env, args.car),
                     cdr: makeLispObject[LispNull]())
   else:
     var cdr = LispList(args.cdr)
-    return LispList(car: eval(args.car),
-                    cdr: parseLambdaList(cdr))
+    return LispList(car: eval(env, args.car),
+                    cdr: parseLambdaList(env, cdr))
 
-proc eval(obj: LispT): LispT =
+proc eval(env: LispEnvironment,
+          obj: LispT): LispT =
   if isNil(obj):
     raise newException(Exception, "nil!!")
 
@@ -58,13 +62,13 @@ proc eval(obj: LispT): LispT =
       return op.function
 
     if op.name == "setq":
-      return evalSetq(args)
+      return evalSetq(env, args)
 
     if op.name == "cond":
       return nil
 
     else:
-      var lambdaList = parseLambdaList(args)
+      var lambdaList = parseLambdaList(env, args)
       return lambdaList
 
   else:
