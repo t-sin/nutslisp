f0da78766dffaa8a1939d4b62b1f8423f697d8da
t-sin <shinichi.tanaka45@gmail.com>
2017-12-24 16:07:28 +0900

    Moves cons functions (Hyperspec section 14: Conses)

---
+++ scl/core/cons.nim
@@ -0,0 +1,21 @@
+import objects
+import runtime
+
+
+proc atom*(obj: LispT): bool =
+  if obj of LispCons:
+    return false
+  else:
+    return true
+
+proc lisp_atom*(rt, LispRuntime, obj: LispT): LispT =
+  nil
+
+proc lisp_car*(rt: LispRuntime, c: LispCons): LispT =
+  return c.car
+
+proc lisp_cdr*(rt: LispRuntime, c: LispCons): LispT =
+  return c.cdr
+
+proc lisp_cons*(rt: LispRuntime, obj1: LispT, obj2: LispT): LispT =
+  return LispCons(car: obj1, cdr: obj2)
--- scl/core/pure.nim
+++ scl/core/pure.nim
@@ -1,20 +1,3 @@
 import macros
 import strutils
 
-import objects
-import runtime
-
-proc lisp_atom*(rt, LispRuntime, obj: LispT): bool =
-  if obj of LispCons:
-    return false
-  else:
-    return true
-
-proc lisp_car*(rt: LispRuntime, c: LispCons): LispT =
-  return c.car
-
-proc lisp_cdr*(rt: LispRuntime, c: LispCons): LispT =
-  return c.cdr
-
-proc lisp_cons*(rt: LispRuntime, obj1: LispT, obj2: LispT): LispT =
-  return LispCons(car: obj1, cdr: obj2)
