0e3ab0c017194af579d45ef674205fb855f895b1
t-sin <shinichi.tanaka45@gmail.com>
2017-12-24 15:54:06 +0900

    Moves eq (Hyperspec section 5: Data and Control Flow)

--- scl/core/pure.nim
+++ scl/core/pure.nim
@@ -10,44 +10,6 @@ proc lisp_atom*(rt, LispRuntime, obj: LispT): bool =
   else:
     return true
 
-template objAreTyped(t: untyped): untyped =
-  obj1 of t and obj2 of t
-
-macro eqReturn(t: untyped, eqexp: untyped): typed =
-  result = newNimNode(nnkStmtList)
-  for n in 1..2:
-    var
-      varDef = newNimNode(nnkVarSection)
-      identDef = newNimNode(nnkIdentDefs)
-      callExp = newNimNode(nnkCall)
-
-    callExp.add(t)
-    callExp.add(newIdentNode("obj" & $(n)))
-    identDef.add(newIdentNode("o" & $(n)))
-    identDef.add(newEmptyNode())
-    identDef.add(callExp)
-    varDef.add(identDef)
-    result.add(varDef)
-
-  var
-    returnStmt = newNimNode(nnkReturnStmt)
-  returnStmt.add(eqexp)
-  result.add(returnStmt)
-
-proc eq*(obj1: LispT, obj2: LispT): bool =
-  if objAreTyped(LispNull):
-    eqReturn(LispNull, true)
-
-  else:
-    return obj1.id == obj2.id
-
-proc lisp_eq*(obj1: LispT, obj2: LispT): LispT =
-  var b = eq(obj1, obj2)
-  if b:
-    return makeLispObject[LispT]()
-  else:
-    return makeLispObject[LispNull]()
-
 proc lisp_car*(rt: LispRuntime, c: LispCons): LispT =
   return c.car
 
