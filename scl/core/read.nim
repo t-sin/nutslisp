bbbd34c59632fcb784878464f3ca5ef08ded4af8
t-sin <shinichi.tanaka45@gmail.com>
2017-12-31 10:19:03 +0900

    Adds skeltons for reader

---
+++ scl/core/read.nim
@@ -0,0 +1,24 @@
+import objects
+import basic_streams
+import readtable
+import runtime
+
+
+proc readDelimitedList(ch: LispCharacter,
+                       inputStream: LispInputStream,
+                       recursiveP: bool): LispList =
+  discard
+
+proc readPreservingWhitespace(inputStream: LispinputStream,
+                              eofErrorP: bool,
+                              eofErrorValue: LispT,
+                              recursiveP: bool): LispCharacter =
+  discard
+
+proc read(inputStream: LispinputStream,
+                              eofErrorP: bool,
+                              eofErrorValue: LispT,
+                              recursiveP: bool): LispCharacter =
+  discard
+
+proc readFromString(str: string)
