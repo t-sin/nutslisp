4989b5d25d32e7b0ff849c358566b3a3499fac99
t-sin <shinichi.tanaka45@gmail.com>
2018-01-03 16:00:34 +0900

    Implements UTF-8 decoding

--- scl/core/basic_streams.nim
+++ scl/core/basic_streams.nim
@@ -43,19 +43,25 @@ proc charseqLength(ch: char): int =
     bitmask = 0b10000000
   while bitmask >= 0b00010000 and ((target and bitmask) != 0):
     count += 1
-    target = target shl 1
+    target = target shr 1
   return count
 
-proc decodeCodepoint(chars: array[char]): int64 =
+proc decodeCodepoint(chars: string): LispCodepoint =
   case chars.len
   of 1:
-    nil
+    return 0b01111111 and ord(chars[0])
   of 2:
-    discard
+    return (`shl`(0b0001111 and ord(chars[0]), 6) or
+            0b00111111 and ord(chars[1]))
   of 3:
-    discard
+    return (`shl`(0b00001111 and ord(chars[0]), 12) or
+            `shl`(0b00111111 and ord(chars[1]), 6) or
+            0b00111111 and ord(chars[2]))
   of 4:
-    discard
+    return (`shl`(0b00000111 and ord(chars[0]), 18) or
+            `shl`(0b00111111 and ord(chars[1]), 12) or
+            `shl`(0b00111111 and ord(chars[2]), 6) or
+            0b00111111 and ord(chars[3]))
   else:
     raise newException(Exception, "malformed utf-8 chars")
 
