82f8380f15d2294c72fa2ec883a21709ca4beac8
t-sin <shinichi.tanaka45@gmail.com>
2018-01-20 16:08:45 +0900

    read parenthesis, but stream has bug??

--- src/markpkg/scl/nl/read.nim
+++ src/markpkg/scl/nl/read.nim
@@ -1,10 +1,35 @@
+import sequtils
+
 import objects
 import basic_streams
 import print
+import utf8
 
+
+proc nl_read*(s: LispCharacterInputStream): LispT
 
 proc readParenthesis(s: LispCharacterInputStream): LispT =
-  return makeLispObject[LispNull]()
+  var
+    cp: LispCodepoint
+    eof: bool
+    list = makeLispObject[LispList]()
+    tail = list
+
+  while true:
+    (cp, eof) = internal_readElem(s, false)
+
+    if eof:
+      raise newException(Exception, "read error while parsing through in list")
+
+    elif cp == ord(')'):
+      tail.cdr = makeLispObject[LispNull]()
+      return list
+
+    else:
+      let val = nl_read(s)
+      tail.car = val
+      tail.cdr = makeLispObject[LispList]()
+      tail = LispList(tail.cdr)
 
 proc readConstituent(s: LispCharacterInputStream): LispT =
   discard
@@ -15,16 +40,18 @@ proc nl_read*(s: LispCharacterInputStream): LispT =
     eof: bool
 
   (cp, eof) = internal_readElem(s, false)
+
   if eof:
     return makeLispObject[LispNull]()
 
   case cp
   of ord('('):
     return readParenthesis(s)
+
   else:
     return makeLispObject[LispNull]()
 
 when isMainModule:
-  let s = makeLispCharacterInputStream(64)
+  let s = makeLispCharacterInputStream(64, toSeq(decodeBytes("(a)")))
 
-  echo print.write(readParenthesis(s))
+  echo write(nl_read(s))
