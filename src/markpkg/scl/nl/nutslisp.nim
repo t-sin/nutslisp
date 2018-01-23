c30dbf03fdacc5a1e333ad815d1322a64c63425b
t-sin <shinichi.tanaka45@gmail.com>
2018-01-23 23:45:33 +0900

    Simplifies definition for native functions

--- src/markpkg/scl/nl/nutslisp.nim
+++ src/markpkg/scl/nl/nutslisp.nim
@@ -1,4 +1,6 @@
+import macros
 import sequtils
+import strutils
 import tables
 
 import objects
@@ -19,6 +21,15 @@ proc initKeywordPackage*(rt: LispRuntime): LispPackage =
   rt.packageTable[pkgName] = pkg
   return pkg
 
+macro defNativeFun(name: untyped): untyped =
+  var source = ""
+  source.add("s = intern(\"$name\", pkg)[0]\n".format("name", name))
+  source.add("fn = makeLispObject[LispFunction]()\n")
+  source.add("fn.nativeP = true\n")
+  source.add("fn.nativeBody = nl_$name\n".format("name", name.strVal))
+  source.add("s.function = fn\n")
+  return parseStmt(source)
+
 proc initNlCorePackage*(rt: LispRuntime): LispPackage =
   var
     pkgName = "nl"
@@ -28,11 +39,7 @@ proc initNlCorePackage*(rt: LispRuntime): LispPackage =
     s: LispSymbol
     fn: LispFunction
 
-  s = intern("eq", pkg)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.nativeP = true
-  fn.nativeBody = nl_eq
-  s.function = fn
+  defNativeFun("eq")
 
   rt.packageTable[pkgName] = pkg
 
