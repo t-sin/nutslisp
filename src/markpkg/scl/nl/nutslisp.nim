f6490c3b7772cca5868d10a096d2b2fdb4c3d13d
t-sin <shinichi.tanaka45@gmail.com>
2018-01-25 22:38:38 +0900

    Defines `eq` function for nuts lisp

--- src/markpkg/scl/nl/nutslisp.nim
+++ src/markpkg/scl/nl/nutslisp.nim
@@ -21,25 +21,23 @@ proc initKeywordPackage*(rt: LispRuntime): LispPackage =
   rt.packageTable[pkgName] = pkg
   return pkg
 
-macro defNativeFun(name: untyped): untyped =
-  var source = ""
-  source.add("s = intern(\"$name\", pkg)[0]\n".format("name", name))
-  source.add("fn = makeLispObject[LispFunction]()\n")
-  source.add("fn.nativeP = true\n")
-  source.add("fn.nativeBody = nl_$name\n".format("name", name.strVal))
-  source.add("s.function = fn\n")
-  return parseStmt(source)
-
 proc initNlCorePackage*(rt: LispRuntime): LispPackage =
   var
     pkgName = "nuts-lisp"
     pkg = initPackage(pkgName, @[])
 
+  rt.currentPackage = pkg
+
   var
     s: LispSymbol
     fn: LispFunction
 
-  defNativeFun("eq")
+  s = intern("eq", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_eq
+  s.function = fn
 
   rt.packageTable[pkgName] = pkg
 
