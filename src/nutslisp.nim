c0ffe05ecba21e857ed50f56cfcf4c79a2c4b28a
t-sin <shinichi.tanaka45@gmail.com>
2018-01-28 09:47:57 +0900

    Moves bootstapping code into new module

--- src/nutslisp.nim
+++ src/nutslisp.nim
@@ -1,91 +1,20 @@
-import macros
 import sequtils
 import strutils
-import tables
 
-import nutslisppkg.objects
-import nutslisppkg.nl_eval
-import nutslisppkg.nl_print
-import nutslisppkg.nl_pure
-import nutslisppkg.nl_read
-import nutslisppkg.nl_runtime
-import nutslisppkg.nl_streams
 import nutslisppkg.utf8
-
-
-proc initKeywordPackage*(rt: LispRuntime): LispPackage =
-  var
-    pkgName = "keyword"
-    pkg = initPackage(pkgName, @[])
-
-  rt.packageTable[pkgName] = pkg
-  return pkg
-
-proc initNlCorePackage*(rt: LispRuntime): LispPackage =
-  var
-    pkgName = "nuts-lisp"
-    pkg = initPackage(pkgName, @[])
-
-  rt.currentPackage = pkg
-
-  var
-    s: LispSymbol
-    fn: LispFunction
-
-  s = intern("eq", rt.currentPackage)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.lambdaList = nil
-  fn.nativeP = true
-  fn.nativeBody = nl_eq
-  s.function = fn
-
-  s = intern("atom", rt.currentPackage)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.lambdaList = nil
-  fn.nativeP = true
-  fn.nativeBody = nl_atom
-  s.function = fn
-
-  s = intern("car", rt.currentPackage)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.lambdaList = nil
-  fn.nativeP = true
-  fn.nativeBody = nl_car
-  s.function = fn
-
-  s = intern("cdr", rt.currentPackage)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.lambdaList = nil
-  fn.nativeP = true
-  fn.nativeBody = nl_cdr
-  s.function = fn
-
-  s = intern("cons", rt.currentPackage)[0]
-  fn = makeLispObject[LispFunction]()
-  fn.lambdaList = nil
-  fn.nativeP = true
-  fn.nativeBody = nl_cons
-  s.function = fn
-
-  rt.packageTable[pkgName] = pkg
-
-  return pkg
-
-proc initNlRuntime*(): LispRuntime =
-  let
-    rt = initRuntime()
-    corePkg = initNlCorePackage(rt)
-
-  rt.currentPackage = corePkg
-  discard initKeywordPackage(rt)
-
-  return rt
+import nutslisppkg.objects
+import nutslisppkg.nl_streams
+import nutslisppkg.nl_print
+import nutslisppkg.nl_read
+import nutslisppkg.nl_eval
+import nutslisppkg.bootstrap
 
 
 let nutslisp_logo* = """
  ⣀⡀ ⡀⢀ ⣰⡀ ⢀⣀   ⡇ ⠄ ⢀⣀ ⣀⡀
  ⠇⠸ ⠣⠼ ⠘⠤ ⠭⠕   ⠣ ⠇ ⠭⠕ ⡧⠜
 """
+
 
 when not defined(javascript):
   proc readFromStdin(s: LispStream) =
@@ -118,6 +47,7 @@ when not defined(javascript):
   when isMainModule:
     echo nutslisp_logo
     nl_repl()
+
 
 when defined(javascript):
   var rt = initNlRuntime()
---
+++ src/nutslisppkg/bootstrap.nim
@@ -0,0 +1,80 @@
+import macros
+import tables
+
+import objects
+import nl_eval
+import nl_print
+import nl_pure
+import nl_read
+import nl_runtime
+import nl_streams
+import utf8
+
+
+proc initKeywordPackage*(rt: LispRuntime): LispPackage =
+  var
+    pkgName = "keyword"
+    pkg = initPackage(pkgName, @[])
+
+  rt.packageTable[pkgName] = pkg
+  return pkg
+
+proc initNlCorePackage*(rt: LispRuntime): LispPackage =
+  var
+    pkgName = "nuts-lisp"
+    pkg = initPackage(pkgName, @[])
+
+  rt.currentPackage = pkg
+
+  var
+    s: LispSymbol
+    fn: LispFunction
+
+  s = intern("eq", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_eq
+  s.function = fn
+
+  s = intern("atom", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_atom
+  s.function = fn
+
+  s = intern("car", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_car
+  s.function = fn
+
+  s = intern("cdr", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_cdr
+  s.function = fn
+
+  s = intern("cons", rt.currentPackage)[0]
+  fn = makeLispObject[LispFunction]()
+  fn.lambdaList = nil
+  fn.nativeP = true
+  fn.nativeBody = nl_cons
+  s.function = fn
+
+  rt.packageTable[pkgName] = pkg
+
+  return pkg
+
+proc initNlRuntime*(): LispRuntime =
+  let
+    rt = initRuntime()
+    corePkg = initNlCorePackage(rt)
+
+  rt.currentPackage = corePkg
+  discard initKeywordPackage(rt)
+
+  return rt
