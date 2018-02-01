d69201e32b5cb9bb3ebe613f9e81c6a153e3f487
t-sin <shinichi.tanaka45@gmail.com>
2018-02-01 23:31:21 +0900

    Byebye, macros, I was so fun *idle activity* with you.

--- src/nutslisppkg/nl_pure.nim
+++ src/nutslisppkg/nl_pure.nim
@@ -4,32 +4,6 @@ import objects
 import nl_runtime
 
 
-template objAreTyped(t: untyped): untyped =
-  obj1 of t and obj2 of t
-
-macro eqReturn(t: untyped, eqexp: untyped): typed =
-  result = newNimNode(nnkStmtList)
-  for n in 1..2:
-    var
-      varDef = newNimNode(nnkVarSection)
-      identDef = newNimNode(nnkIdentDefs)
-      callExp = newNimNode(nnkCall)
-
-    result.add(macros.parseStmt("{.push hint[XDeclaredButNotUsed]: off.}"))
-    callExp.add(t)
-    callExp.add(newIdentNode("obj" & $(n)))
-    identDef.add(newIdentNode("o" & $(n)))
-    identDef.add(newEmptyNode())
-    identDef.add(callExp)
-    varDef.add(identDef)
-    result.add(varDef)
-    result.add(macros.parseStmt("{.pop.}"))
-
-  var
-    returnStmt = newNimNode(nnkReturnStmt)
-  returnStmt.add(eqexp)
-  result.add(returnStmt)
-
 proc eq*(obj1: LispT, obj2: LispT): bool =
   return obj1.id == obj2.id
 
