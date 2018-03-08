f538fc3acbcbcf806442972ab8281034a97d8fe7
t-sin <shinichi.tanaka45@gmail.com>
2018-03-09 00:47:22 +0900

    Implements `intern`

--- src/nutslisppkg/nl_packages.nim
+++ src/nutslisppkg/nl_packages.nim
@@ -11,3 +11,24 @@ proc makePackage*(name: string,
   result.nicknames = nicknames
   result.nicknames = uses
   result.symbols = newTable[string, LispSymbol]()
+
+proc intern*(name: string,
+             package: LispPackage): (LispSymbol, string) =
+  for n in package.symbols.keys:
+    if n == name:
+      let s = package.symbols[n]
+
+      if isNil(s.package):
+        raise newException(Exception, "invalid symbol: " & s.package.name & ":" & s.name)
+      if s.package.name == package.name:
+        return (s, "internal")
+      if s.package.name == "keyword":
+        return (s, "external")
+      else:
+        return (s, "inherited")
+
+  let s = makeLispObject[LispSymbol]()
+  s.name = name
+  s.package = package
+  package.symbols[s.name] =s
+  return (s, nil)
